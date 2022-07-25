48ac6bf82b4aa9e9aae7757b8f7cb464866f8dbd
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-25 15:31:12 +0530

    Added program to find the max of integers

---
+++ src/com/bridgelabz/Generics/MainClass.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Generics;
+
+public class MainClass {
+    public static void main(String[] args) {
+        IntMax intmax = new IntMax();
+        System.out.println("Test case 1 :");
+        intmax.intMax(1,2,3);
+        System.out.println("Test case 2 :");
+        intmax.intMax(4,9,6);
+        System.out.println("Test case 3 :");
+        intmax.intMax(8,10,5);
+    }
+}
